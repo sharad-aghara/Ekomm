fae937bdd09c730a8675c20bae62a6b1adc96dfe
Sharad Aghara <sharadaghara24@gmail.com>
2024-06-26 12:47:52 +0530

    commented

--- src/routes/checkoutRoute.js
+++ src/routes/checkoutRoute.js
@@ -9,28 +9,28 @@ const router = express.Router();
 // Checkout route
 router.post(
     '/checkout',
-    [
-        check('name')
-            .not()
-            .isEmpty()
-            .withMessage('Name is required'),
-        check('address')
-            .not()
-            .isEmpty()
-            .withMessage('Address is required'),
-        check('cardNumber')
-            .not()
-            .isEmpty()
-            .withMessage('Card number is required'),
-        check('expirationDate')
-            .not()
-            .isEmpty()
-            .withMessage('Expiration date is required'),
-        check('cvv')
-            .not()
-            .isEmpty()
-            .withMessage('CVV is required')
-    ],
+    // [
+    //     check('name')
+    //         .not()
+    //         .isEmpty()
+    //         .withMessage('Name is required'),
+    //     check('address')
+    //         .not()
+    //         .isEmpty()
+    //         .withMessage('Address is required'),
+    //     check('cardNumber')
+    //         .not()
+    //         .isEmpty()
+    //         .withMessage('Card number is required'),
+    //     check('expirationDate')
+    //         .not()
+    //         .isEmpty()
+    //         .withMessage('Expiration date is required'),
+    //     check('cvv')
+    //         .not()
+    //         .isEmpty()
+    //         .withMessage('CVV is required')
+    // ],
     Verify,
     Validate,
     Checkout
